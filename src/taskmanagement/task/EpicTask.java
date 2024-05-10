62518fe2b25ef6ac9857350e0f564c2c7d59969c
DmitryFedoroff <fedoroffx@gmail.com>
2024-05-10 22:07:55 +0700

    feat: Organize EpicTask with subtask tracking
    #comment Organize EpicTask to manage a collection of subtasks and maintain their overall status
    
    Affected: EpicTask

---
+++ src/taskmanagement/task/EpicTask.java
@@ -0,0 +1,36 @@
+package taskmanagement.task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EpicTask extends BaseTask {
+    private final List<Integer> subtaskIds;
+
+    public EpicTask(String title, String description) {
+        super(title, description);
+        this.subtaskIds = new ArrayList<>();
+    }
+
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    public void addSubtask(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public void removeSubtask(int subtaskId) {
+        subtaskIds.remove(Integer.valueOf(subtaskId));
+    }
+
+    @Override
+    public String toString() {
+        return "EpicTask{" +
+                "id=" + getId() +
+                ", title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+}
